84c1e29486c657d8a7c9eabac19773f5b93d7951
阿怪 <862704559@qq.com>
2019-01-15 18:49:05 +0800

    Update mapGrid.vue

--- src/components/mapGrid.vue
+++ src/components/mapGrid.vue
@@ -12,34 +12,12 @@ export default {
       // 百度地图API功能
       var map = new BMap.Map('allmap')
       var localSearch = new BMap.LocalSearch(map)
-      var that = this
-      localSearch.setSearchCompleteCallback((searchResult)=>{
-        // 获取指定城市 经纬度
-        // var poi = searchResult.getPoi(0);
-        // that.lng = poi.point.lng
-        // that.lat = poi.point.lat
-
-        // 创建地图实例 传递经纬度
-        var point = new BMap.Point(that.lng, that.lat)
-
-        // var point = new BMap.Point(118.798398,32.072485)
-        // 创建点坐标
-        map.centerAndZoom(point, 12)
-        // 初始化地图， 设置中心点坐标和地图级别
-        var marker = new BMap.Marker(point)
-        map.addOverlay(marker)
-      })
-      localSearch.search(this.city)
-    }
-  },
-  computed: {
-    Lng() {
-      return  this.lng
-    }
-  },
-  watch: {
-    Lng(val) {
-      this.ready()
+      var point = new BMap.Point(this.lng, this.lat)
+      // 创建点坐标
+      map.centerAndZoom(point, 12)
+      // 初始化地图， 设置中心点坐标和地图级别
+      var marker = new BMap.Marker(point)
+      map.addOverlay(marker)
     }
   }
 }
