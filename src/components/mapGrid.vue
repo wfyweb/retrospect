787316c625a932e3453e5ec1d41fd4bfba8d5fc6
阿怪 <862704559@qq.com>
2019-01-15 18:51:43 +0800

    修改地图显示问题

--- src/components/mapGrid.vue
+++ src/components/mapGrid.vue
@@ -13,7 +13,6 @@ export default {
     ready () {
       // 通过经纬度显示地图
       var map = new BMap.Map('allmap')
-      var localSearch = new BMap.LocalSearch(map)
       // 创建点坐标
       var point = new BMap.Point(this.lng, this.lat)
       // 初始化地图， 设置中心点坐标和地图级别
